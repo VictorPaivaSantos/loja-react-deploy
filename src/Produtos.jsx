465d2288054924b3544b9c711c1e0abf43ffa628
Victor Paiva dos Santos <victor.santos@ccc.ufcg.edu.br>
2023-08-30 11:17:42 -0300

    mais atualizações de imagens

--- src/Produtos.jsx
+++ src/Produtos.jsx
@@ -2,7 +2,7 @@
 const produtos = [{
     id: 1,
     nome: "Cadeira",
-    imagem: "../imagens/cadeira.jpeg",
+    imagem: "/loja-react-deploy/imagens/cadeira.jpeg",
     preco: 999.99,
     descricao: "Descrição do produto.",
     cores: ["Preto", "Branco"],
@@ -13,7 +13,7 @@ const produtos = [{
 {
     id: 2,
     nome: "Microfone",
-    imagem: "../imagens/microfone.jpeg",
+    imagem: "/loja-react-deploy/imagens/microfone.jpeg",
     preco: 99.99,
     descricao: "Descrição do produto.",
     cores: [],
@@ -22,7 +22,7 @@ const produtos = [{
 {
     id: 3,
     nome: "Computador",
-    imagem: "../imagens/pc.jpeg" ,
+    imagem: "/loja-react-deploy/imagens/pc.jpeg" ,
     preco: 2999.99,
     descricao: "Descrição do produto.",
     cores: [],
@@ -31,7 +31,7 @@ const produtos = [{
 {
     id: 4,
     nome: "Celular",
-    imagem: "../imagens/celular.jpeg" ,
+    imagem: "/loja-react-deploy/imagens/celular.jpeg" ,
     preco: 4499.99,
     descricao: "Descrição do produto.",
     cores: ["Preto", "Branco"],
@@ -40,7 +40,7 @@ const produtos = [{
 {
     id: 5,
     nome: "Fone",
-    imagem: "../imagens/fone.jpeg" ,
+    imagem: "/loja-react-deploy/imagens/fone.jpeg" ,
     preco: 89.99,
     descricao: "Descrição do produto.",
     cores: [],
@@ -49,7 +49,7 @@ const produtos = [{
 {
     id: 6,
     nome: "Mouse",
-    imagem: "../imagens/mouse.jpeg" ,
+    imagem: "/loja-react-deploy/imagens/mouse.jpeg" ,
     preco: 119.99,
     descricao: "Descrição do produto.",
     cores: [],
@@ -58,7 +58,7 @@ const produtos = [{
 {
     id: 7,
     nome: "Teclado",
-    imagem: "../imagens/teclado.jpeg" ,
+    imagem: "/loja-react-deploy/imagens/teclado.jpeg" ,
     preco: 239.99,
     descricao: "Descrição do produto.",
     cores: [],
@@ -67,7 +67,7 @@ const produtos = [{
 {
     id: 8,
     nome: "Processador",
-    imagem: "../imagens/processador.jpeg" ,
+    imagem: "/loja-react-deploy/imagens/processador.jpeg" ,
     preco: 2789.99,
     descricao: "Descrição do produto.",
     cores: [],
@@ -76,7 +76,7 @@ const produtos = [{
 {
     id: 9,
     nome: "Memoria",
-    imagem: "../imagens/memoria.jpeg" ,
+    imagem: "/loja-react-deploy/imagens/memoria.jpeg" ,
     preco: 279.99,
     descricao: "Descrição do produto.",
     cores: [],
@@ -85,7 +85,7 @@ const produtos = [{
 {
     id: 10,
     nome: "Placa Mãe",
-    imagem: "../imagens/placa-mae.jpeg" ,
+    imagem: "/loja-react-deploy/imagens/placa-mae.jpeg" ,
     preco: 799.99,
     descricao: "Descrição do produto.",
     cores: [],
@@ -94,7 +94,7 @@ const produtos = [{
 {
     id: 11,
     nome: "Placa de Vídeo",
-    imagem: "../imagens/placa-de-video.jpeg" ,
+    imagem: "/loja-react-deploy/imagens/placa-de-video.jpeg" ,
     preco: 4999.99,
     descricao: "Descrição do produto.",
     cores: [],
@@ -103,7 +103,7 @@ const produtos = [{
 {
     id: 12,
     nome: "Fonte",
-    imagem: "../imagens/fonte.jpeg" ,
+    imagem: "/loja-react-deploy/imagens/fonte.jpeg" ,
     preco: 799.99,
     descricao: "Descrição do produto.",
     cores: [],
@@ -112,7 +112,7 @@ const produtos = [{
 {
     id: 13,
     nome: "Gabinete",
-    imagem: "../imagens/gabinete.jpeg" ,
+    imagem: "/loja-react-deploy/imagens/gabinete.jpeg" ,
     preco: 349.99,
     descricao: "Descrição do produto.",
     cores: [],
@@ -121,7 +121,7 @@ const produtos = [{
 {
     id: 14,
     nome: "Monitor",
-    imagem: "../imagens/monitor.jpeg" ,
+    imagem: "/loja-react-deploy/imagens/monitor.jpeg" ,
     preco: 1499.99,
     descricao: "Descrição do produto.",
     cores: [],
@@ -130,7 +130,7 @@ const produtos = [{
 {
     id: 15,
     nome: "Water Cooler",
-    imagem: "../imagens/wc.jpeg" ,
+    imagem: "/loja-react-deploy/imagens/wc.jpeg" ,
     preco: 379.99,
     descricao: "Descrição do produto.",
     cores: [],
@@ -139,7 +139,7 @@ const produtos = [{
 {
     id: 16,
     nome: "Notebook",
-    imagem: "../imagens/notebook.jpeg" ,
+    imagem: "/loja-react-deploy/imagens/notebook.jpeg" ,
     preco: 4899.99,
     descricao: "Descrição do produto.",
     cores: [],
